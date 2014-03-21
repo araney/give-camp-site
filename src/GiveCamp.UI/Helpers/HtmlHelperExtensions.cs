65ba9c394c9a766ec775bccd3374a139f59d7381
araney <araney@dwhomes.com>
2014-03-21 14:19:28 -0500

    Added Updates
    Removed custom containers and replaced with styled panels

---
+++ src/GiveCamp.UI/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiveCamp.UI.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static PanelHelper BeginPanel(this HtmlHelper htmlHelper)
+        {
+            return new PanelHelper(htmlHelper);
+        }
+
+        public static PanelHelper BeginPanel(this HtmlHelper htmlHelper, string heading)
+        {
+            return new PanelHelper(htmlHelper, heading);
+        }
+    }
+}
---
+++ src/GiveCamp.UI/Helpers/PanelHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiveCamp.UI.Helpers
+{
+    public class PanelHelper : IDisposable
+    {
+        private readonly HtmlHelper _htmlHelper;
+
+        public PanelHelper(HtmlHelper htmlHelper, string heading = null)
+        {
+            _htmlHelper = htmlHelper;
+
+            const string panelFormat = @"<div class=""panel"">{0}<div class=""panel-body"">";
+            const string panelHeadingFormat = @"<div class=""panel-heading""><h3 class=""panel-title"">{0}</h3></div>";
+
+            var headingOutput = (heading != null)
+                ? string.Format(panelHeadingFormat, heading)
+                : string.Empty;
+
+            var output = string.Format(panelFormat, headingOutput);
+
+            htmlHelper.ViewContext.Writer.Write(output);
+        }
+
+        public void Dispose()
+        {
+            _htmlHelper.ViewContext.Writer.Write("</div></div>");
+        }
+    }
+}
